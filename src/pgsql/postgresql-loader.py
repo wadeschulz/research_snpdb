a7831955463654ad551fc83f114fc18439435544
Wade Schulz, MD, PhD <wade.schulz@gmail.com>
2016-08-16 17:09:35 -0400

    cleanup/refactor pgsql

--- src/pgsql/postgresql-loader.py
+++ src/pgsql/postgresql-loader.py
@@ -1,23 +1,29 @@
+#!/usr/bin/env python
 import argparse
 import csv, os, time
 import psycopg2  # psycopg2 v2.5.1
-import gspread, getpass # https://pypi.python.org/pypi/gspread/ (v0.1.0)
-import sys
 
+import sys
 sys.path.append('../modules')
 from result import Result
 
+__author__ = "Wade Schulz, Donn Felker, Brent Nelson"
+__credits__ = ["Wade Schulz", "Donn Felker", "Brent Nelson"]
+__license__ = "MIT"
+__version__ = "2.0.0"
+__maintainer__ = "Wade Schulz"
+__email__ = "wade.schulz@gmail.com"
+__status__ = "Research"
+
+
 # Get command line arguments
-parser = argparse.ArgumentParser(description='Load SNP and locus data')
+parser = argparse.ArgumentParser(description='Load SNP and locus data into PostgreSQL')
 parser.add_argument('--dev', action='store_true', help='Only load chromosome 21 for development testing')
-parser.add_argument('--path', help='Path to chromosome data')
-parser.add_argument('--db', type=str, help='Postgres database name')
-parser.add_argument('--yhost', type=str, help='Postgres host')
-parser.add_argument('--username', type=str, help='Postgres username')
-parser.add_argument('--password', type=str, help='Postgres password')
+parser.add_argument('--db', type=str, help='PostgreSQL database name')
+parser.add_argument('--username', type=str, help='PostgreSQL username')
+parser.add_argument('--password', type=str, help='PostgreSQL password')
 parser.add_argument('--tag', type=str, help='Tag to place in results file')
-parser.add_argument('--remote', action='store_true', help='Enable remote reporting')
-parser.add_argument('--rkey', help='Google document key')
+parser.add_argument('--path', help='Path to chromosome data (dev: ../../res/; otherwise default working directory)')
 parser.add_argument('--start', type=str, help='Chromosome to start load from')
 parser.add_argument('--indexes', action='store_true', help='Create indexes')
 parser.add_argument('--queries', action='store_true', help='Run queries')
@@ -28,27 +34,20 @@ scriptVersion = "2.0"
 
 # Set default variables
 dev = False
-remote = False
-createIndexes = False
-runQueries = False
 databaseName = 'snp_research'
 username = 'dev'
 password = ''
-sqlHost = '127.0.0.1'
-path = ''
 tag = ''
-docKey = ''
+path = ''
 start = '1'
+createIndexes = False
+runQueries = False
 
 # Update any present from CLI
 if args.dev: # If dev mode, only load chr 21
     dev = True
-if args.remote and args.rkey is not None: # If set to remote log and document key is present, log to GDocs
-    remote = True
-    docKey = args.rkey
-else:
-    remote = False
-    
+    path = '../../res/'
+
 if args.path is not None: # If set, use as root path for chromosome data
     path = args.path
 if args.db is not None: # If set, use as database name for Postgres
@@ -57,8 +56,6 @@ if args.username is not None: # Postgres username
     username = args.username
 if args.password is not None: # Postgres password
     password = args.password
-if args.yhost is not None: # Postgres host name
-    sqlHost = args.yhost
 if args.tag is not None: # Tag to place in results file
     tag = args.tag
 if args.start is not None:
@@ -68,8 +65,8 @@ if args.indexes is not None:
 if args.queries is not None:
     runQueries = args.queries
     
-# Open results file
-resultsFileName = 'results-postgres'
+# Open results file and print header
+resultsFileName = 'results-pgsql'
 if resultsFileName != "":
     resultsFileName += '-' + tag
 resultsFileName += '.txt'
@@ -77,15 +74,6 @@ resultsFile = open(resultsFileName, 'w')
 resultsFile.write(scriptVersion + '\n')
 result = Result()
 resultsFile.write(result.toHeader() + '\n')
-
-if remote:
-    gusername = raw_input("Enter Google username: ")
-    gpassword = getpass.getpass("Enter Google password: ")    
-    gs = gspread.Client(auth=(gusername,gpassword))
-    gs.login()
-    ss = gs.open_by_key(docKey)
-    ws = ss.add_worksheet(tag + "-" + str(time.time()),1,1)
-    ws.append_row(result.headerArr())
 
 # Data files
 snpFilePath = 'snpData-chr{0}.txt'
@@ -108,6 +96,7 @@ if dev is False:
         chromosomes = startList
 
 # Create Postgres database, tables if not exists
+# For initial connection, connect to user database and then create the experimental db
 postgresConnection = psycopg2.connect("dbname=" + username + " user=" + username)
 postgresConnection.autocommit = True
 createDbCursor = postgresConnection.cursor()
@@ -116,6 +105,7 @@ createDbCursor.execute("CREATE DATABASE " + databaseName)
 createDbCursor.close()
 postgresConnection.close() # Reconnect with database name
 
+# Reopen connection to the experimental database to create tables and begin inserts
 postgresConnection = psycopg2.connect("dbname=" + databaseName + " user=" + username)
 createDbCursor = postgresConnection.cursor()
 
@@ -141,6 +131,7 @@ for name, ddl in TABLES.iteritems():
     createDbCursor.execute(ddl)
     postgresConnection.commit()
 
+# Disable triggers/constraints on tables
 createDbCursor.execute("ALTER TABLE snp DISABLE trigger ALL;")
 createDbCursor.execute("ALTER TABLE locus DISABLE trigger ALL;")
 
@@ -152,6 +143,7 @@ lociInserts = []   # Array for loci insert queries
 rsidList = {}      # Dictionary of RSIDs that will also hold the 
                    # primary key for each SNP in SQL
 
+# Load each chromosome into database
 for curChr in chromosomes:
     result = Result()
     result.method = "pgsql"
@@ -164,19 +156,20 @@ for curChr in chromosomes:
     curLociFilePath = lociFilePath.format(curChr)
     
     if len(path) > 0:
-        curSnpFilePath = path.rstrip('\\') + '\\' + curSnpFilePath
-        curLociFilePath = path.rsplit('\\') + '\\' + curLociFilePath
+        curSnpFilePath = path.rstrip('\\').rstrip('/') + '\\' + curSnpFilePath
+        curLociFilePath = path.rstrip('\\').rstrip('/') + '\\' + curLociFilePath
     
     # Clear dictionaries for loading multiple chromosomes
     snpInserts.clear()
     lociInserts = []
     rsidList.clear()
 
+    # Print status and flush stdout for nohup
     print "Chromosome " + str(curChr) + ". Reading SNP Data"
     result.snpLoadStart = time.time()
     sys.stdout.flush()
 
-    # Read in data from SNP file
+    # Read in data from SNP file and create insert statements
     with open(curSnpFilePath,'r') as csvfile:
         data = csv.reader(csvfile,delimiter='\t')
         for row in data:
@@ -193,7 +186,7 @@ for curChr in chromosomes:
     result.totalSnps = len(snpInserts)
            
     # Insert SNP data into postgres
-    createDbCursor = postgresConnection.cursor()
+    cursor = postgresConnection.cursor()
 
     print "Chromosome " + str(curChr) + ". Inserting SNP Data."
     sys.stdout.flush()
@@ -203,15 +196,15 @@ for curChr in chromosomes:
     
     # For each snp, insert record and then grab primary key
     for rsid,snp in snpInserts.iteritems():
-        createDbCursor.execute(snp)
-        rsidList[rsid] = createDbCursor.fetchone()[0]
+        cursor.execute(snp)
+        rsidList[rsid] = cursor.fetchone()[0]
         
     # Commit all inserts to pgsql and grab end time
     postgresConnection.commit()
     
     # Log completed time, close pgsql cursor
     result.snpInsertEnd=time.time()
-    createDbCursor.close()
+    cursor.close()
 
     # Clear list of SNPs to free up memory
     snpInserts.clear()
@@ -258,22 +251,17 @@ for curChr in chromosomes:
     resultsFile.write(result.toString() + '\n')
     sys.stdout.flush()
 
-    if remote:
-        try:
-            print "Sending to GDocs..."
-            gs.login()
-            ws.append_row(result.stringArr())
-        except:
-            print "Unable to send to GDocs, continuing..."
-
 # Create new cursor, create indexes and run test queries
 cursor = postgresConnection.cursor()    
 
+# Turn on triggers, create FK index since PGSQL does not
+# automatically index FKs
 print "Turning on key checks..."
 cursor.execute("ALTER TABLE snp ENABLE trigger ALL;")
 cursor.execute("ALTER TABLE locus ENABLE trigger ALL;")
 cursor.execute("CREATE INDEX idx_snpid_fk ON locus (snp_id)")
 
+# Create indexes if requested in arguments
 if createIndexes:
     result = Result()
     result.method = "pgsql-Idx"
@@ -286,35 +274,29 @@ if createIndexes:
     print "Creating RSID index..."
     idxStart = time.time()
     cursor.execute(rsidIndex)
-    cursor.commit()
+    postgresConnection.commit()
     idxEnd = time.time()
     result.idxRsid = idxEnd - idxStart
     
     print "Creating ClinSig index..."
     idxStart = time.time()
     cursor.execute(clinIndex)
-    cursor.commit()
+    postgresConnection.commit()
     idxEnd = time.time()
     result.idxClinSig = idxEnd - idxStart        
 
     print "Creating Gene index..."
     idxStart = time.time()
     cursor.execute(geneIndex)
-    cursor.commit()
+    postgresConnection.commit()
     idxEnd = time.time()
     result.idxGene = idxEnd - idxStart
 
     resultsFile.write(result.toString() + '\n')
-    if remote:
-        try:
-            print "Sending to GDocs..."
-            gs.login()
-            ws.append_row(result.stringArr()) 
-        except:
-            print "Unable to send to GDocs, continuing..."
-       
+
+# Run queries if requested in args 
 if runQueries:
-    for z in range(1,101):
+    for z in range(1,11):
         result = Result()
         result.method = "pgsql-Qry" + str(z)
         result.tag = tag
@@ -341,13 +323,6 @@ if runQueries:
         result.qryByGeneSig = idxEnd - idxStart        
 
         resultsFile.write(result.toString() + '\n')
-        if remote:
-            try:
-                print "Sending to GDocs..."
-                gs.login()
-                ws.append_row(result.stringArr()) 
-            except:
-                print "Unable to send to GDocs, continuing..."
 
 # Close pgsql cursor
 cursor.close()
